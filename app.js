937f5612c2b146bb54bf2601c1cdf3a370cd2447
victor tung <vtfsdev@gmail.com>
2020-10-30 13:30:47 -0500

    Added connection to MongoDB

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var config = require('./config.dev');
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var config = require('./config.dev');
+
+//Connect to MongoDB
+mongoose.connect(config.mongodb, { useNewUrlParser: true });
 
 //Test the file
 //console.log(config);

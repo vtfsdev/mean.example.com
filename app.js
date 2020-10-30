5fe68e181772981f1b8a6f4906d59a4eea9fdd00
victor tung <vtfsdev@gmail.com>
2020-10-30 13:23:24 -0500

    Created a sitewide config file

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+var config = require('./config.dev');
+
+//Test the file
+//console.log(config);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

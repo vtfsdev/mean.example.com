dd9068657908bddca999b57e0820362f06458bc3
victor tung <vtfsdev@gmail.com>
2020-10-30 15:31:14 -0500

    Added endpoint for logging out of the session

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -62,4 +62,14 @@ router.post('/login', function(req, res, next) {
     })(req, res, next);
 });
 
+router.delete('/logout', function(req, res){
+    req.session.destroy(function (err) {
+      if(err){
+        return res.json({success: 'false'});
+      }else{
+        return res.json({success: 'true'});
+      }
+    });
+});
+
 module.exports = router;

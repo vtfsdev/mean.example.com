712e552328033585375be2225e50cc8b6e671495
victor tung <vtfsdev@gmail.com>
2020-11-04 11:37:28 -0600

    Get the data for the selected user

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -146,6 +146,25 @@ var usersApp = (function() {
         }
     }
 
+    function editUser(id){
+
+        let uri = `${window.location.origin}/api/users/${id}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', uri);
+      
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+      
+        xhr.send();
+      
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          console.log(data);
+        }
+    }
+
     function postRequest(formId, url){
         let form = document.getElementById(formId);
         form.addEventListener('submit', function(e){
@@ -196,7 +215,8 @@ var usersApp = (function() {
                 break;
           
               case '#edit':
-                console.log('EDIT');
+                editUser(hashArray[1]);
+                //console.log('EDIT');
                 break;
           
               case '#delete':

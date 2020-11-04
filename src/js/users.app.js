bf6cdd91b7297c1f2ad9647811254e487d3fc50c
victor tung <vtfsdev@gmail.com>
2020-11-04 11:10:40 -0600

    Mocked-up the navigation

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -63,8 +63,30 @@ var usersApp = (function() {
 
     return {
         load: function(){
-          // alert('LOADED');
-          viewUsers();
+            let hash = window.location.hash;
+            let hashArray = hash.split('-');
+          
+            switch(hashArray[0]){
+              case '#create':
+                console.log('CREATE');
+                break;
+          
+              case '#view':
+                console.log('VIEW');
+                break;
+          
+              case '#edit':
+                console.log('EDIT');
+                break;
+          
+              case '#delete':
+                console.log('DELETE');
+                break;
+          
+              default:
+                viewUsers();
+                break;
+            }
         }
     }
   

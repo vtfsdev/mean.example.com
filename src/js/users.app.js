b020d7f2d2b0a0c3c4d9697e9f7b1a3d65cf53e6
victor tung <vtfsdev@gmail.com>
2020-11-04 11:56:20 -0600

    Added user delete

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -200,7 +200,10 @@ var usersApp = (function() {
                         <input type="email" id="email" name="email" class="form-control" value="${data.user.email}" required>
                       </div>
                     </div>
-          
+
+                    <div>
+                        <a href="#delete-${data.user._id}" class="text-danger">Delete</a>
+                    </div>
                     <div class="text-right">
                       <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
                     </div>
@@ -210,10 +213,56 @@ var usersApp = (function() {
             `;
           
             app.innerHTML=form;
+            processRequest('editUser', '/api/users', 'PUT');
         }
     }
 
-    function postRequest(formId, url){
+    function deleteView(id){
+
+        let uri = `${window.location.origin}/api/users/${id}`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', uri);
+      
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+      
+        xhr.send();
+      
+        xhr.onload = function(){
+          let app = document.getElementById('app');
+          let data = JSON.parse(xhr.response);
+          let card = '';
+      
+          card = `<div class="card bg-transparent border-danger text-danger bg-danger">
+            <div class="card-header bg-transparent border-danger">
+              <h2 class="h3 text-center">Your About to Delete a User</h2>
+            </div>
+            <div class="card-body text-center">
+              <div>
+                Are you sure you want to delete
+                <strong>${data.user.first_name} ${data.user.last_name}</strong>
+              </div>
+      
+              <div>Username: <strong>${data.user.username}</strong></div>
+              <div>Email: <strong>${data.user.email}</strong></div>
+      
+              <div class="text-center">
+                <br>
+                <a class="btn btn-lg btn-danger text-white">
+                  Yes delete ${data.user.username}
+                </a>
+              </div>
+      
+            </div>
+          </div>`;
+      
+          app.innerHTML = card;
+        }
+    }
+
+    function processRequest(formId, url, method){
         let form = document.getElementById(formId);
         form.addEventListener('submit', function(e){
           e.preventDefault();
@@ -221,7 +270,7 @@ var usersApp = (function() {
           let formData = new FormData(form);
           let uri = `${window.location.origin}${url}`;
           let xhr = new XMLHttpRequest();
-          xhr.open('POST', uri);
+          xhr.open(method, uri);
     
           xhr.setRequestHeader(
             'Content-Type',
@@ -253,7 +302,7 @@ var usersApp = (function() {
             switch(hashArray[0]){
               case '#create':
                 createUser();
-                postRequest('createUser', '/api/users');
+                processRequest('createUser', '/api/users', 'POST');
                 //console.log('CREATE');
                 break;
           
@@ -268,7 +317,8 @@ var usersApp = (function() {
                 break;
           
               case '#delete':
-                console.log('DELETE');
+                deleteView(hashArray[1]);
+                //console.log('DELETE');
                 break;
           
               default:

a75c46bab4c9c8d15e49e7d5b26a1db07eb96958
victor tung <vtfsdev@gmail.com>
2020-11-04 15:34:01 -0600

    Pulled a list of articles from the API

--- src/js/articles.app.js
+++ src/js/articles.app.js
@@ -1,8 +1,28 @@
 var articlesApp = (function() {
 
+    function viewArticles(){
+
+        let uri = `${window.location.origin}/api/articles`;
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', uri);
+      
+        xhr.setRequestHeader(
+          'Content-Type',
+          'application/json; charset=UTF-8'
+        );
+      
+        xhr.send();
+      
+        xhr.onload = function(){
+          let data = JSON.parse(xhr.response);
+          console.log(data);
+        }
+    }
+
     return {
       load: function(){
-        alert('LOADED');
+        //alert('LOADED');
+        viewArticles();
       }
     }
   
